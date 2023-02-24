4b3fc26d8cfc9d2a602f0ff2dacad9e92f841486
pratheektek <pratheekp10@gmail.com>
2023-02-24 10:36:22 +0530

    typecasting

---
+++ src/javaTest/Typecasting.java
@@ -0,0 +1,26 @@
+package javaTest;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+byte b=100;
+int a =65;
+
+b=(byte) a;
+System.out.println(b);
+
+
+long l=a;
+char ch='a';
+System.out.println(ch);
+
+int x=ch;
+System.out.println(x);
+
+int y=119;
+char z=(char) y;
+System.out.println(z);
+	}
+
+}
