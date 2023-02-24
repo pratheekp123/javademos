d3ecf7ab07df39cca34f4c3c53bd9b759b6ef9a0
pratheektek <pratheekp10@gmail.com>
2023-02-24 18:23:59 +0530

    testing

---
+++ src/Test2.java
@@ -0,0 +1,33 @@
+
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		for(int i= 0; i<=10;i++) {
+			
+			System.out.println(i);
+		}
+		
+		for(int j=10;j>=0;j--) {
+			System.out.println(j);
+		}
+for(int k=0;k<=7;k++) {
+	System.out.println("test");
+	if(k<=5) {
+		break;
+	}
+}
+
+int sum=0;
+for(int m=0;m<=10;m++) {
+	sum +=m;
+	System.out.println(sum);
+}
+	
+	}
+	
+
+}
